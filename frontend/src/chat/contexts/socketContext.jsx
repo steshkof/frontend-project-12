b0236f12f171c7f1c8edda21063eaaab21eeb938
steshkof <steshkof@gmail.com>
2023-03-20 17:13:22 +0300

    20.03

--- frontend/src/chat/contexts/socketContext.jsx
+++ frontend/src/chat/contexts/socketContext.jsx
@@ -4,21 +4,38 @@ import { useDispatch } from 'react-redux';
 
 const SocketContext = createContext({});
 
+const connectionTimeout = 3000; 
+const emitWithConnetionTimeout = (socket, event, data) => {
+  new Promise((resolve, reject) => {
+    const logMessages = {
+      newMessage: {
+        success: "Message sent. Server response:",
+        error: "Server did't response within timeout"
+      }
+    }
+
+    const timer = setTimeout(() => {
+      reject(new Error("Server did't response within timeout"))
+    },connectionTimeout)
+
+    socket.emit(event, data, (response) => {
+      if (response.status === 'ok') {
+        clearTimeout(timer);
+        resolve(console.log(logMessages[event].success, response))
+      } else {
+        reject(new Error(`${logMessages[event].error}. Trying event: ${event}`))
+      }
+    });
+  })
+}
+
 const SocketProvider = ({ socket, children }) => {
   const dispatch = useDispatch();
-    
+
   socket.on('newMessage', (payload) => dispatch(addMessage(payload)));
 
-
-
-
   const socketProviderValue = {
-    sendMessage: (value) => socket.emit('newMessage', value, (err, response) => {
-      console.log(response);
-      if(response?.status) {
-        console.log('Ошибка отправки сообщения!', response?.status)
-      }
-    }),
+    sendMessage: (data) => emitWithConnetionTimeout(socket, 'newMessage', data)
   };
 
   return (
